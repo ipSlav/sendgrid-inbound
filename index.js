b230e649c4744232b00c5f15aab06387bba4db81
ipSlav <63005335+ipSlav@users.noreply.github.com>
2022-01-27 18:40:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-const express = require('express')
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/<your_domain>/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/<your_domain>/cert.pem', 'utf8');
+var fullChain = fs.readFileSync('/etc/letsencrypt/live/<your_domain>/fullchain.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate, ca: fullChain};
+const express = require('express');
 const multer = require('multer')
 
 const app = express()
 const upload = multer()
 
-app.post('/api/parse', upload.any(), async (req, res) => {
+app.post('/<your_>/<_endpoint>', upload.any(), async (req, res) => {
 
     const body = req.body
-    const rawFullMimeMessageChecked = false
 
-    console.log("dkim: ", body.dkim);
-    console.log("to: ", body.to);
-    console.log("cc: ", body.cc);
-    console.log("from: ", body.from);
-    console.log("subject: ", body.subject);
-    console.log("sender_ip: ", body.sender_ip);
-    console.log("spam_report: ", body.spam_report);
-    console.log("envelope: ", body.envelope);
-    console.log("charsets: ", body.charsets);
-    console.log("SPF: ", body.SPF);
-    console.log("spam_score: ", body.spam_score);
-
-    // Logs properties 
-    if (rawFullMimeMessageChecked) {
-        console.log("email: ", body.email);
-    } else {
-        console.log("headers: ", body.headers);
-        console.log("html: ", body.html);
-        console.log("text: ", body.text);
-        console.log("attachments: ", body.attachments);
-        console.log("attachment-info: ", body['attachment-info']);
-        console.log("content-ids: ", body['content-ids']);
-    }
-
+    console.log("['sender_ip']=> ", body.sender_ip, "\n");
+    console.log("['dkim']=> ", body.dkim, "\n");
+    console.log("['SPF']=> ", body.SPF, "\n");
+    console.log("['spam_report']=> ", body.spam_report, "\n");
+    console.log("['spam_score']=> ", body.spam_score, "\n");
+    console.log("['headers']=> ", body.headers, "\n");
+    console.log("['charsets']=> ", body.charsets, "\n");
+    console.log("['html']=> ", body.html, "\n");
+    console.log("['text']=> ", body.text, "\n");
+    console.log("['attachments']=> ", body.attachments, "\n");
+    console.log("['attachment-info']=> ", body['attachment-info'], "\n");
+    console.log("['content-ids']=> ", body['content-ids'], "\n");
+    
     if (req.files.length > 0) {
         // Log file data
         console.log(req.files)
@@ -43,6 +38,7 @@ app.post('/api/parse', upload.any(), async (req, res) => {
     return res.status(200).send()
 })
 
-app.listen(3000, () => {
-    console.log('Webserver running on port 3000! -> http://localhost:3000')
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(<port>, '0.0.0.0', () => {
+    console.log('Webserver running on port <port>! -> https://0.0.0.0:<port>')
 })
